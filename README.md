2febf8ae64163ab3ad56ce1be62b7eb653768624
jviictor <joaovictorrrr93@gmail.com>
2022-06-02 00:10:20 -0300

    graficos

---
+++ README.md
@@ -0,0 +1,6 @@
+Instalar biblioteca de grafico
+
+npm add chart.js react-chartjs-2
+
+rodar 
+npm start
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
+import { UserData } from "./Data";
+
+function App() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1",
+          "#ecf0f1",
+          "#50af95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor:"black",
+        borderWidth: 2,
+      },
+    ],
+  });
+  return (
+    <div className="App">
+      <div style={{ width: 400 }}>
+        <BarChart chartData={userData} />
+      </div>
+      <div style={{ width: 400 }}>
+        <LineChart chartData={userData} />
+      </div>
+      <div style={{ width: 400 }}>
+        <PieChart chartData={userData} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
